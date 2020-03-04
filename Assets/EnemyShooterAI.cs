e471e78f022ce23773d93750402c8258aca02a61
Yashu7 <janmalawski2@gmail.com>
2020-03-04 22:56:57 +0100

    Movement

--- Assets/EnemyShooterAI.cs
+++ Assets/EnemyShooterAI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyShooterAI : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class EnemyShooterAI : MonoBehaviour
     public CharacterController controller;
     public State enemyState = State.Patroling;
     public Transform Player;
+    public Quaternion myRotation;
+
+    
 
     public float distance;
 
@@ -17,25 +21,40 @@ public class EnemyShooterAI : MonoBehaviour
         controller = gameObject.GetComponent<CharacterController>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+
         distance = Vector3.Distance(Player.position, transform.position);
-        if(distance <= 30 && distance > 15)
+        if(distance <= 30 && distance > 5)
         {
-            Patrolling();
+            MoveTowards();
 
         }
-        if(distance <= 15)
+        if(distance <= 5)
         {
             Shooting();
         }
+
+
     }
-    public void Patrolling()
+    public void MoveTowards()
     {
-        transform.position = Vector3.MoveTowards(transform.position, Player.position, 5f * Time.deltaTime);
+        Rotate(Player);
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(Player.position.x, transform.position.y, Player.position.z), 5f * Time.deltaTime);
+        //gameObject.GetComponent<Rigidbody>().MovePosition(Player.position + transform.right * Time.fixedDeltaTime);
+        gameObject.GetComponent<NavMeshAgent>().transform.LookAt(Player);
+        gameObject.GetComponent<NavMeshAgent>().destination = Player.position;
+
     }
     public void Shooting()
     {
+        Rotate(Player);
         Debug.Log("SHooting");
+    }
+
+    public void  Rotate(Transform followPosition)
+    {
+        myRotation = Quaternion.LookRotation(followPosition.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, myRotation, 10 * Time.deltaTime);
     }
 }
